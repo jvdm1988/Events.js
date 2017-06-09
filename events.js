fa7e635fce5a5baa273d0cd40f411002c8f5d2f2
Jessicavandermarel <vandermareljessica@gmail.com>
2017-06-09 16:54:04 -0400

    done

--- events.js
+++ events.js
@@ -1,7 +1,6 @@
 console.log("EVENTS.JS IS WORKING");
 
 $(document).ready(function () {
-
   $("#my-checkbox").change(function () {
     var isChecked = $ ("#my-checkbox").prop("checked");
 
@@ -11,5 +10,36 @@ $(document).ready(function () {
     else {
       $("#pizza-img").hide ();
     }
+  });
+});
+
+$(document).ready(function () {
+
+  var leftAmount = 0;
+  var topAmount = 0;
+
+//keydown, keyup, keypress
+  $(document).keydown(function (theEvent) {
+    // theEvent.preventDefault();
+    //up arrow key
+    if (theEvent.which === 38) {
+        topAmount -= 15;
+    }
+    //right arrow key
+    else if (theEvent.which === 39){
+        leftAmount += 15;
+    }
+    //down arrow key
+    else if (theEvent.which === 40){
+        topAmount += 15;
+    }
+    //left arrow key
+    else if (theEvent.which === 37){
+        leftAmount -= 15;
+    }
+
+$(".literally-everything").css("left", leftAmount + "px");
+$(".literally-everything").css("top", topAmount + "px");
+$(".literally-everything").css("position", "relative");
   });
 });
